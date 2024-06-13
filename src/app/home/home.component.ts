4092c485bbb5ff9b15a98b2f776d00bc1ec29cc8
Yorane Abbou <yorane.a@gmail.com>
2024-06-13 15:59:36 +0200

    Création Home + init Form Modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ModalComponent } from '../shared/ui/modal/modal.component';
+import { Checklist } from '../shared/insterfaces/checklist';
 
 @Component({
-  selector: 'app-home',
   standalone: true,
-  imports: [],
+  selector: 'app-home',
   template: `
-    <p>
-      home works!
-    </p>
+    <header>
+      <h1>Quicklists</h1>
+      <button (click)="checklistBeingEdited.set({})">Add Checklist</button>
+    </header>
+
+    <app-modal [isOpen]="!!checklistBeingEdited()">
+      <ng-template> Modal </ng-template>
+    </app-modal>
   `,
-  styles: ``
+  imports: [ModalComponent],
 })
 export default class HomeComponent {
-
+  checklistBeingEdited = signal<Partial<Checklist> | null>(null);
 }
